d54cbc176f7893ee3ed11223395c9d48514d024e
dai-z <runtime.error@qq.com>
2019-04-09 11:04:25 +0800

    Update install(copy file)

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-cp -r ./* $HOME/
+cp -avr ./.config $HOME/ --parents
+cp -avr ./.oh-my-zsh $HOME/ --parents
+cp -avr ./.ssh $HOME/ --parents
+cp -avr ./.zshrc $HOME/ --parents
+cp -avr ./.tmux.conf $HOME/ --parents
+
