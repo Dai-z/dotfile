2cbece61b994c6ff46e999644254a246f108f284
dai-z <runtime.error@qq.com>
2020-01-06 14:35:25 +0800

    Set log level to info

--- service/daiz-lock/synergy-on-lock.sh
+++ service/daiz-lock/synergy-on-lock.sh
@@ -9,7 +9,7 @@ gdbus monitor -y -d org.freedesktop.login1 |
           killall synergy > /dev/null 2> /dev/null
       else
           status "Start synergy on unlock"
-          /usr/bin/synergys -f --no-tray --debug DEBUG --name daiz-PC --enable-crypto --address :24800 -l /tmp/fuck.log --display :1
+          /usr/bin/synergys -f --no-tray --debug INFO --name daiz-PC --enable-crypto --address :24800 -l /tmp/synergy.log --display :1
       fi
     fi
   done
