09e33dd0e7ac4f7555c2b48e9e9d16682c166828
dai-z <runtime.error@qq.com>
2020-11-25 10:14:05 +0800

    Add -9 when killing synergy client on lock

--- service/daiz-lock/synergy-on-lock.sh
+++ service/daiz-lock/synergy-on-lock.sh
@@ -5,8 +5,8 @@ gdbus monitor -y -d org.freedesktop.login1 |
     if [[ $x =~ "LockedHint" ]] ; then
       if [[ $x =~ "true" ]] ; then
           echo "Stop synergy on lock"
-          killall synergys > /dev/null 2> /dev/null
-          killall synergy > /dev/null 2> /dev/null
+          killall -9 synergys > /dev/null 2> /dev/null
+          killall -9 synergy > /dev/null 2> /dev/null
       else
           status "Start synergy on unlock"
           /usr/bin/synergys --daemon --no-tray --debug INFO --name daiz-PC --enable-crypto --address :24800 -l /tmp/synergy.log --display :1
